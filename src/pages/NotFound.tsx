aad125da97b88f3c13464963fdd96ae52824dabd
Ranga Bandara <rangabandara1729@gmail.com>
2025-05-22 17:07:33 +0530

    Ádd NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,7 +6,8 @@ function NotFound() {
     <div className={style.container}>
       <h1 className={style.number}>404</h1>
       <p className={style.mainText}>Page Not Found</p>
-      <p className={style.paragraph}>The page you are looking for doesn't exist or another error occured. Got to <Link to={"/home"}>Home</Link> page.</p>
+      <p className={style.paragraph}>The page you are looking for doesn't exist or another error occured.<br/>
+         Got to <Link to={"/"}>Home</Link> page.</p>
     </div> 
   )
 }
